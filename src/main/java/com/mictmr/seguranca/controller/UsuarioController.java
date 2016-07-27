23638478751610482551032ad55e081439546815
Almir Fontenele <almir.oliveira@supportcomm.com.br>
2016-07-27 16:16:26 -0300

    .

--- src/main/java/com/mictmr/seguranca/controller/UsuarioController.java
+++ src/main/java/com/mictmr/seguranca/controller/UsuarioController.java
@@ -4,9 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
